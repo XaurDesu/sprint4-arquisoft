b9a5fbd5dc35d59bde8d1c0bb429563205614c4b
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-12-08 16:25:32 -0500

    add mongo support

--- src/main.go
+++ src/main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Request struct {
+	Nombre  string
+	Fecha   time.Time
+	Monto   int
+	Plazo   time.Time
+	Empresa string
+}
+
 func main() {
-	fmt.Println("Amogus.")
 	r := gin.Default()
 
-	r.GET("/requests", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI("mongodb+srv://admin:w41I0DHA3EkkYY8V@sprint4.jcenge9.mongodb.net/?retryWrites=true&w=majority").
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.PUT("/requests", func(c *gin.Context) {
+		var request Request
+		c.BindJSON(&request)
+
+		c.JSON(200, request)
+		fmt.Println("Success parsing Request! Sending to DB...")
+		cl := client.Database("requests").Collection("requests")
+		cl.InsertOne(ctx, request)
+		fmt.Println("Sent to mongo!")
 	})
 
-	r.Run()
+	r.Run("0.0.0.0:8080")
 }
